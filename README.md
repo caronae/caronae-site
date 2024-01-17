3d42c5c80719cfc77c4020534caf5b6e4bd25e6b
Nathan Andrade <nathan.nhdsk@gmail.com>
2024-01-16 21:46:14 -0300

    [CAE-25] adiciona atualizações no site antigo

---
+++ README.md
@@ -0,0 +1,4 @@
+# Caronaê - Website
+<img src="https://caronae.org/images/caronae-logo.png" width="40%">
+Código fonte do site Caronaê - 
+https://caronae.org
---
+++ js/get-parameter.js
@@ -0,0 +1,13 @@
+/**
+  * @desc get the value of a parameter in the url
+  * @param string name - name of the parameter
+  * @return string - value of the parameter with the given name
+*/
+function getParameterByName(name) {
+	var url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
